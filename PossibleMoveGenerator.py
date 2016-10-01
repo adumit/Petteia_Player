7299ca2877de66c53b955d85c26e38d56b08b9d6
adumit <andrew.dumit@gmail.com>
2016-10-01 00:11:01 -0500

    PossibleMoveGenerator can generate moves

--- PossibleMoveGenerator.py
+++ PossibleMoveGenerator.py
@@ -6,3 +6,37 @@
 - A single move is represented by a 64x4 matrix where the first two columns are the
 current state of the board and the next two are the state of the board after a move.
 """
+
+class PossibleMoveGenerator():
+
+    def __init__(self):
+
+
+    def generateMoves(self, gameBoard, side):
+        possibleMoves = []
+        for piece in side:
+            # For moving forward
+            for i in range(piece[0]-1, 0, -1):
+                if gameBoard.grid[i][piece[1]] == 0:
+                    possibleMoves.append((piece, (i, piece[1])))
+                else:
+                    break
+            # For moving backwards
+            for i in range(piece[0]+1, 8):
+                if gameBoard.grid[i][piece[1]] == 0:
+                    possibleMoves.append((piece, (i, piece[1])))
+                else:
+                    break
+            # For moving left
+            for i in range(piece[1]-1, 0, -1):
+                if gameBoard.grid[piece[0]][i] == 0:
+                    possibleMoves.append((piece, (piece[0], i)))
+                else:
+                    break
+            # For moving right
+            for i in range(piece[1]+1, 8):
+                if gameBoard.grid[piece[0]][i] == 0:
+                    possibleMoves.append((piece, (piece[0], i)))
+                else:
+                    break
+        return possibleMoves

67755a2facc17ae72ecbc0ec033f995c2ea2265e
Max Hasbrouck <maxhasbro@gmail.com>
2016-09-30 23:53:30 -0500

    built the basic gameboard

--- GameBoard.py
+++ GameBoard.py
@@ -7,3 +7,32 @@ whoever has the most pieces
 - Should store every move for a game from each player
 """
 
+
+class GameBoard:
+
+    def __init__(self):
+        self.grid = [[0 for i in range(8)] for j in range(8)]
+        self.neg = [(0, x) for x in range(8)]
+        self.pos = [(7, x) for x in range(8)]
+        for index in range(8):
+            self.grid[0][index] = -1
+            self.grid[7][index] = 1
+
+    def print_board(self):
+        board_str = ""
+        for i in range(8):
+            line_str = "|"
+            for j in range(8):
+                if self.grid[i][j] == 1:
+                    line_str += " + |"
+                elif self.grid[i][j] == -1:
+                    line_str += " - |"
+                else:
+                    line_str += "   |"
+            board_str += line_str + "\n"
+        print(board_str)
+
+
+
+
+

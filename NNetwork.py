0b1afff5e0a94225489167dd080af6710761f203
Andrew Dumit <Andrew Dumit>
2016-12-12 15:09:33 -0600

    Commit before merge

--- NNetwork.py
+++ NNetwork.py
@@ -27,83 +27,49 @@ def max_pool3d_2x2(x, ksize, strides):
   return tf.nn.max_pool3d(x, ksize=ksize, strides=strides, padding='SAME')
 
 # ------------------- Red-side Network Definition -------------------
-x_red = tf.placeholder(tf.float32, shape=[8, 8, 4])
-y__red = tf.placeholder(tf.float32, shape=[1])
+class ConvLayer(object):
 
-x_image_red = tf.reshape(x_red, [-1, 4, 8, 8, 1])
-
-# first convolutional layer
-W_conv1_red = weight_variable([4, 2, 2, 1, 32], 'W_conv1_red')
-b_conv1_red = bias_variable([32])
-h_conv1_red = tf.nn.relu(conv3d(x_image_red, W_conv1_red, strides=[1,4,1,1,1]) + b_conv1_red)
-h_pool1_red = max_pool3d_2x2(h_conv1_red, ksize=[1, 1, 2, 2, 1], strides=[1, 1, 2, 2, 1])
-
-# second convolutional layer
-W_conv2_red = weight_variable([1, 4, 4, 32, 64], 'W_conv2_red') ### TODO: THIS CALL IS QUESTIONABLE... why 1, 4, 4 as first three args
-b_conv2_red = bias_variable([64])
-h_conv2_red = tf.nn.relu(conv3d(h_pool1_red, W_conv2_red, strides=[1,4,1,1,1]) + b_conv2_red)
-h_pool2_red = max_pool3d_2x2(h_conv1_red, ksize=[1, 1, 2, 2, 1], strides=[1, 1, 2, 2, 1])
-
-
-# densely connected layer
-W_fc1_red = weight_variable([512, 1024], 'W_fc1_red')
-b_fc1_red = bias_variable([1024])
-h_pool2_flat_red = tf.reshape(h_pool2_red, [-1, 512])
-h_fc1_red = tf.nn.relu(tf.matmul(h_pool2_flat_red, W_fc1_red) + b_fc1_red)
-
-# softmax for classifying as good move or bad move
-W_fc2_red = weight_variable([1024, 1], 'W_fc2_red')
-b_fc2_red = bias_variable([1])
-y_conv_red = tf.matmul(h_fc1_red, W_fc2_red) + b_fc2_red
-
-# This loss function should seek to choose the best moves that lead to the best outcome
-# Mutliplying a good move * good outcome will lead to a very large (negative here) number.
-# Which is the best minimization. A high predicted move * a bad outcome will be very bad,
-# which satifies the goal
-loss_func_red = -y_conv_red*y__red
-train_step_red = tf.train.AdamOptimizer(1e-4).minimize(loss_func_red)
-
-# ------------------- Black-side Network Definition -------------------
-x_black = tf.placeholder(tf.float32, shape=[8, 8, 4])
-y__black = tf.placeholder(tf.float32, shape=[1])
-
-x_image_black = tf.reshape(x_black, [-1, 4, 8, 8, 1])
-
-# first convolutional layer
-W_conv1_black = weight_variable([4, 2, 2, 1, 32], 'W_conv1_black')
-b_conv1_black = bias_variable([32])
-h_conv1_black = tf.nn.relu(conv3d(x_image_black, W_conv1_black, strides=[1,4,1,1,1]) + b_conv1_black)
-h_pool1_black = max_pool3d_2x2(h_conv1_black, ksize=[1, 1, 2, 2, 1], strides=[1, 1, 2, 2, 1])
-
-# second convolutional layer
-W_conv2_black = weight_variable([1, 4, 4, 32, 64], 'W_conv2_black') ### TODO: THIS CALL IS QUESTIONABLE... why 1, 4, 4 as first three args
-b_conv2_black = bias_variable([64])
-h_conv2_black = tf.nn.relu(conv3d(h_pool1_black, W_conv2_black, strides=[1,4,1,1,1]) + b_conv2_black)
-h_pool2_black = max_pool3d_2x2(h_conv1_black, ksize=[1, 1, 2, 2, 1], strides=[1, 1, 2, 2, 1])
+    def __init__(self, input, depth, in_channel, out_channel, window, strides, name_suffix):
+        """
+        :param input: Tensorflow variable that is the input to this layer
+        :param depth: Depth to look at for each convolution
+        :param in_channel: Number of channels going into the layer
+        :param out_channel: Number of channels out
+        :param window: A length-2 list of the window to look at
+        :param strides: A length-2 list of the stride size
+        :param name_suffix: Suffix to append to the variable names
+        """
+        self.input = input
+        self.W_conv = weight_variable([depth, window[0], window[1], in_channel, out_channel], name="W" + name_suffix)
+        self.b_conv = bias_variable([out_channel])
+        self.h_conv = tf.nn.relu(conv3d(self.input, self.W_conv, strides=[1, in_channel, 1, 1, 1]) + self.b_conv)
+        self.h_pool = max_pool3d_2x2(self.h_conv, ksize=[1, 1, window[0], window[1], 1], strides=[1, 1, strides[0], strides[1], 1])
 
 
-# densely connected layer
-W_fc1_black = weight_variable([512, 1024], 'W_fc1_black')
-b_fc1_black = bias_variable([1024])
-h_pool2_flat_black = tf.reshape(h_pool2_black, [-1, 512])
-h_fc1_black = tf.nn.relu(tf.matmul(h_pool2_flat_black, W_fc1_black) + b_fc1_black)
+class FCLayer(object):
 
-# softmax for classifying as good move or bad move
-W_fc2_black = weight_variable([1024, 1], 'W_fc2_black')
-b_fc2_black = bias_variable([1])
-y_conv_black = tf.matmul(h_fc1_black, W_fc2_black) + b_fc2_black
-
-# This loss function should seek to choose the best moves that lead to the best outcome
-# Mutliplying a good move * good outcome will lead to a very large (negative here) number.
-# Which is the best minimization. A high predicted move * a bad outcome will be very bad,
-# which satifies the goal
-loss_func_black = -y__black*y_conv_black
-train_step_black = tf.train.AdamOptimizer(1e-4).minimize(loss_func_black)
+    def __init__(self, input, weight_dimensions, name_suffix):
+        self.W_fc = weight_variable(shape=weight_dimensions, name="W" + name_suffix)
+        self.b_fc = bias_variable(shape=[weight_dimensions[1]])
+        self.output = tf.nn.relu(tf.matmul(input, self.W_fc) + self.b_fc)
 
 
-# --------------- Global Tensorflow variables ---------------
-init_op = tf.initialize_all_variables()
-saver = tf.train.Saver()
+class NNetwork(object):
+
+    def __init__(self, color):
+        self.x = tf.placeholder(tf.float32, shape=[None, 8, 8, 4])
+        self.y = tf.placeholder(tf.float32, shape=[1])
+        self.x_image = tf.reshape(self.x, [-1, 4, 8, 8, 1])
+
+        self.layer1 = ConvLayer(self.x_image, 4, 1, 32, [2, 2], [2, 2], "_conv1_" + color)
+        self.layer2 = ConvLayer(self.layer1.h_pool, 1, 32, 64, [2, 2], [2, 2], "_conv2_" + color)
+        self.layer2flattened = tf.reshape(self.layer2.h_pool, [-1, 256])
+        self.layer3 = FCLayer(self.layer2flattened, [256, 512], "_fc1_" + color)
+        self.layer4 = FCLayer(self.layer3.output, [512, 1], "_fc2_" + color)
+
+        self.y_hat = self.layer4.output
+        self.loss = self.y_hat * self.y
+        self.optimizer = tf.train.AdamOptimizer(1e-4).minimize(self.loss)
 
 # --------------- Running the network ---------------
 
@@ -118,14 +84,19 @@ saver = tf.train.Saver()
 if __name__ == "__main__":
     nn_saver_dir = './nn_checkpoints/'
 
-    should_restore = True
+    should_restore = False
     should_write_output_to_file = False
 
-    with tf.Session() as sesh:
+    with tf.Session() as sess:
 
-        sesh.run(init_op)
+        redNetwork = NNetwork("red")
+        blackNetwork = NNetwork("black")
+        init_op = tf.initialize_all_variables()
+        saver = tf.train.Saver()
+
+        sess.run(init_op)
         if should_restore:
-            saver.restore(sesh, './nn_checkpoints/divergence_90_60_random_checkpoint_1000.ckpt')
+            saver.restore(sess, './nn_checkpoints/divergence_90_60_random_checkpoint_1000.ckpt')
             print("Model restored")
 
 
@@ -149,7 +120,7 @@ if __name__ == "__main__":
                 move_scores = np.zeros(len(possible_moves))
                 for i in range(len(possible_moves)):
                     matrix_board = gameboardRed.to_matrix(possible_moves[i])
-                    move_scores[i] = sesh.run(y_conv_red, feed_dict={x_red: matrix_board})
+                    move_scores[i] = sess.run(redNetwork.y_hat, feed_dict={redNetwork.x: matrix_board})
                 # With the best move found, move on both red and black boards
                 rand_choice = random()
                 if (rand_choice < .9):
@@ -173,7 +144,7 @@ if __name__ == "__main__":
                 for i in range(len(possible_moves)):
                     matrix_board = gameboardBlack.to_matrix(possible_moves[i])
                     before_pass = datetime.now()
-                    move_scores[i] = sesh.run(y_conv_red, feed_dict={x_red: matrix_board})
+                    move_scores[i] = sess.run(blackNetwork.y_hat, feed_dict={blackNetwork.x: matrix_board})
                     if should_write_output_to_file:
                         if i == 0:
                             print(datetime.now() - before_pass)
@@ -219,12 +190,12 @@ if __name__ == "__main__":
             black_score = -red_score
 
             for move in moves_made_red:
-                train_step_red.run(feed_dict={x_red: move, y__red: np.array([red_score])})
+                redNetwork.optimizer.run(feed_dict={redNetwork.x: move, redNetwork.y: np.array([red_score])})
             for move in moves_made_black:
-                train_step_black.run(feed_dict={x_black: move, y__black: np.array([black_score])})
+                blackNetwork.optimizer.run(feed_dict={blackNetwork.x: move, blackNetwork.y: np.array([black_score])})
             print("Time for one game: " + str(datetime.now() - start_time))
 
             if games_played % 200 == 0:
-                saver.save(sesh, nn_saver_dir + 'divergence_90_60_random_checkpoint_' + str(games_played + 1000) + '.ckpt')
+                saver.save(sess, nn_saver_dir + 'divergence_90_60_random_checkpoint_' + str(games_played + 1000) + '.ckpt')
             games_played += 1
 
